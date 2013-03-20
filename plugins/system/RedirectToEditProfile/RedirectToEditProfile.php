728033cbcbdb34859e2de769ba129f63674aba09
Manish <manish@readybytes.in>
2013-03-21 01:52:28 +0530

    ##No more support for Joomla1.5. ## Compatibility With Joomla 1.6 +

--- plugins/system/RedirectToEditProfile/RedirectToEditProfile.php
+++ plugins/system/RedirectToEditProfile/RedirectToEditProfile.php
@@ -1,10 +1,17 @@
-<?php
+ <?php
+
+/*
+* Author 	: Team JoomlaXi @ Ready Bytes Software Labs Pvt. Ltd.
+* Email 	: manish@readybytes.in
+* License 	: GNU-GPL V2
+* (C) www.joomlaxi.com
+*/
 
 defined('_JEXEC') or die('Restricted access to this plugin'); 
 
 jimport('joomla.plugin.plugin');
 
-class plgSystemRedirecttoEditProfile extends JPlugin
+class plgSystemRedirectToEditProfile extends JPlugin
 {
 	function __construct(& $subject, $params )
 	{
@@ -13,229 +20,145 @@ class plgSystemRedirecttoEditProfile extends JPlugin
 	
 	function onAfterRoute()
 	{
-		global $mainframe;
-		
-		if($mainframe->isAdmin())
-			return;
-		
-		if($this->isComponentExists('community') === false)
-			return;
-			
-		$option = JRequest::getCmd('option');
-		$view 	= JRequest::getCmd('view');
-		$task 	= JRequest::getCmd('task');
-	
-		if($option == 'com_community' && $view == 'profile' && $task == 'edit')
-			return;
-	
-		if($option == 'com_community' && $view == 'profile' && $task == 'editDetails')
-			return;
-	
-		if($option == 'com_user' && $task == 'logout')
-			return;
-			
-		if($option == 'com_login' || $option == 'com_acctexp')
-			return;
-		
-		$userid 	=& JFactory::getUser()->id;
-	
-		// For Guest, do nothing and just return, let the joomla handle it
-		if(!$userid) 
-			return;
-		
-		// if admin then return 
-		if($this->isSuperAdministrator())
-			return;
-		
-		//when user profile is complete (already cached)
-		if($this->userParameter($userid,'profilecomplete','get'))
-			return;
-	
-		$plugin =& JPluginHelper::getPlugin('system', 'RedirectToEditProfile');
- 		$params = new JParameter($plugin->params);
+		$params = $this->params;
 	
 		//whenredirect 0 means redirect only at login else everytime
-		$whenredirect = $params->get('whenredirect',0);
-		$whichfield = $params->get('whichfield',0);
-		$message  = $params->get('message');
+		$whenredirect	= $params->get('whenredirect',0);
+		$whichfield 	= $params->get('whichfield',0);
+		$message  		= $params->get('message');
 	
-		if($whenredirect == 0 && !$this->userParameter($userid,'justloggedin','get'))
-			return;
+		if(!$whenredirect) { // After login action will be handled by onuserlogin event
+			return true; 
+		}
 	
-		$this->userParameter($userid,'justloggedin','unset');
-	
-		$url = CRoute::_('index.php?option=com_community&view=profile&task=edit',false);
-	
-		if (!$this->checkCompleteField($userid,$whichfield))
-			$mainframe->redirect( $url,$message);
+		
+		// Check initial conditions
+		if(!self::_isApplicable()) {
+			return false;
+		}
+				
+		if (self::_isRedirectRequired(JFactory::getUser()->id, $whichfield)) {
+			$url = CRoute::_('index.php?option=com_community&view=profile&task=edit',false);
+			JFactory::getApplication()->redirect( $url,$message);
+		}
 	}
 
 
-	function onLoginUser($user,$option)
+	function onUserLogin($user,$option)
 	{
-		global $mainframe;
+		$userId		= JUserHelper::getUserId($user['username'] );
 		
-		if($mainframe->isAdmin())
-			return;
+		// Check initial conditions
+		if(!self::_isApplicable($userId)) {
+			return false;
+		}
 
-		if($this->isComponentExists('community') === false)
-			return;
+		$params = $this->params;
+
+		$whichfield = $params->get('whichfield',0);
+		$message  	= $params->get('message');
+
+		if (self::_isRedirectRequired($userId, $whichfield)) {
+			$url = CRoute::_('index.php?option=com_community&view=profile&task=edit',false);
+			//JFactory::getApplication()->redirect( $url,$message);
+			// Handle itself by joomla....by this way we dont conflict into existing Joomla login flow or into other extensions
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($message);
+			$app->setUserState('users.login.form.return', $url);
+		}
+		return true;
+	}
+
+
+	/**
+	 * Check initial condition, Plugin task applicable or not
+	 */
+	private static function _isApplicable($userId = null)
+	{
+		$app = JFactory::getApplication();
+		
+		if ($app->isAdmin()) {
+			return false;
+		}
+
+		// Not applicable when Community does not exist 
+		if(!self::_isComponentExists('community')) {
+			//XiTODO:: System msg for disable this plugin
+			return false;
+		}
+		
+		$input = $app->input;
+		
+		$option = $input->get('option');
+		$view 	= $input->get('view');
+		$task 	= $input->get('task');
+	
+		$user	= JFactory::getUser($userId);
+		$userid = $user->id;
+		
+		// Not Applicable conditions
+		if (	 
+				( $option == 'com_community' && $view == 'profile' && ( $task == 'edit' || $task == 'editDetails' )) ||
+				( $option == 'com_users' && $task == 'user.logout') ||
+				( $option == 'com_login' || $option == 'com_acctexp') ||
+				( !$userid ) || ( $user->get('isRoot') )				// if user have root permission 
+			) 
+			{	return false; }
 			
-		$option = JRequest::getCmd('option');
-		$view 	= JRequest::getCmd('view');
-		$task 	= JRequest::getCmd('task');
-	
-		$userid 	=& JFactory::getUser($user['username'])->id;
-		
-		// For Guest, do nothing and just return, let the joomla handle it
-		if(!$userid) 
-			return;
-	
-		// if admin then return
-		if($this->isSuperAdministrator())
-			return;
-	
-		$this->userParameter($userid,'justloggedin','set');
-	
-		$plugin =& JPluginHelper::getPlugin('system', 'RedirectToEditProfile');
- 		$params = new JParameter($plugin->params);
-	
-		$whichfield = $params->get('whichfield',0);
-	
-		if ($this->checkCompleteField($userid,$whichfield))
-			$this->userParameter($userid,'profilecomplete','set');
-		else
-			$this->userParameter($userid,'profilecomplete','unset');
+		return true;
 	}
-
-
-	function getProfile($userid)
+	
+	/**
+	 * Return user Profile
+	 * @param $userid
+	 */
+	private static function _getProfile($userid)
 	{
+		//XiTODO:: Include appropriate location
+		// include Community API
+		require_once (JPATH_ROOT. DS.'components'.DS.'com_community'.DS.'libraries'.DS.'core.php');
+		
 		$pModel = CFactory::getModel('profile');
 		$profile = $pModel->getEditableProfile($userid);
-		return $profile['fields'];
+		return $profile;
 	}
 
-	function checkCompleteField($userid,$required)
+	/**
+	 * Check redirection required or not as per plugin params
+	 * @param $userid
+	 * @param $required
+	 */
+	private static function _isRedirectRequired($userid, $required)
 	{	
-		global $mainframe;
+		$profile	= self::_getProfile($userid);
+		$fields 	= $profile['fields'];
 		
-		$db =& JFactory::getDBO();
-		$count = 0;
-		$empty = 0;
-		$fields = $this->getProfile($userid);
-		$pModel = CFactory::getModel('profile');
-	
-		foreach($fields as $name => $fieldGroup)
-		{
-			foreach($fieldGroup as $field)
-			{
-				if($required == 1 && $field['required'])
-					$whichfield = 1;
-				else if($required == 1 && !$field['required'])
-					$whichfield = 0;
-				else if($required == 0)
-					$whichfield = 1;
-			
-				if($whichfield)
-				{
-					$count++;
-					if($pModel->_fieldValueExists($field['fieldcode'],$userid))
-					{
-						$query = 'SELECT value FROM #__community_fields_values' . ' '
-						. 'WHERE `field_id`=' .$db->Quote( $field['id'] ) . ' '
-						. 'AND `user_id`=' . $db->Quote( $userid );
-
-						$db->setQuery( $query );
-						if($db->getErrorNum()) {
-							JError::raiseError( 500, $db->stderr());
-						}
-
-						$result = $db->loadResult();
-						if(empty($result))
-							return false; 
+		foreach ($fields as $name => $fieldGroup) {
+			foreach ($fieldGroup as $field) {
+				if ( !$required || $field['required']) {
+					if (empty($field['value'])) {
+						return true;					// if we get any field empty
 					}
-					else
-						return false;
 				}
 			}
 		}
 	
-		return true;
+		return false;
 	}
 	
-	/* function set_user_parameter($userid,$paramname)
-	{
-		$db			=& JFactory::getDBO();
-		$user =& JUser::getInstance((int)$userid);
-		$user->setParam($paramname, '1');
-		$user->save();
-	}
-
-	function unset_user_parameter($userid,$paramname)
-	{
-		$db			=& JFactory::getDBO();
-		$user =& JUser::getInstance((int)$userid);
-		$user->setParam($paramname, '0');
-		$user->save();
-	}
-
-	function get_user_parameter($userid,$paramname)
-	{
-		$user =& JUser::getInstance((int)$userid);
-		$profilecomplete	= $user->getParam($paramname,'0');
-		return $profilecomplete;
-	}*/
-	function userParameter($userid,$paramname,$status)
-	{
-		if($status =='get')
-		{
-			$user =& JUser::getInstance((int)$userid);
-			$profilecomplete	= $user->getParam($paramname,'0');
-			return $profilecomplete;
-		}
-		else
-		{
-			$db			=& JFactory::getDBO();
-			$user =& JUser::getInstance((int)$userid);
-			if($status =='set')
-			{
-				$user->setParam($paramname, '1');
-				$user->save();
-				return;
-			}
-			if($status =='unset')
-			{
-				$user->setParam($paramname, '0');
-				$user->save();
-				return;
-			}
-		}
-	}
-	
-	
-	function isSuperAdministrator($userid = null)
-	{
-		$loggedinUser	= JFactory::getUser($userid);
-		if(!$loggedinUser)
-			return false;
-					
-		return ( $loggedinUser->usertype == 'Super Administrator' || $loggedinUser->usertype == 'Administrator');
-	}
-	
-	
-	function isComponentExists($comName)
+	/**
+	 * Check component exist or not
+	 * @param $comName
+	 */
+	private static function _isComponentExists($comName)
 	{
 		$comSitePath	=	JPATH_ROOT . DS . 'components' .DS.	'com_' . $comName;
 		$comAdminPath	=	JPATH_ADMINISTRATOR . DS . 'components' .DS.	'com_' . $comName;
 		
-		jimport('joomla.filesystem.folder');
-		
-		if(!JFolder::exists($comSitePath) || !JFolder::exists($comAdminPath))
+		if(!JFolder::exists($comSitePath) || !JFolder::exists($comAdminPath)) {
 			return false;
-			
-		require_once (JPATH_ROOT. DS.'components'.DS.'com_community'.DS.'libraries'.DS.'core.php');
+		}
+		
 		return true;
 	}
 }
-?>
